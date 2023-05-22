98ef508508fd4dfdb168fa2a5b262926df344bd2
Ulisses Duo <duoulisses@gmail.com>
2023-05-22 07:36:56 -0300

    function select payments

---
+++ js/areUserConnected.js
@@ -0,0 +1,90 @@
+import {
+	getAuth,
+	onAuthStateChanged,
+} from "https://www.gstatic.com/firebasejs/9.17.1/firebase-auth.js"
+
+import {
+	getDatabase,
+	ref,
+	onValue,
+} from "https://www.gstatic.com/firebasejs/9.17.1/firebase-database.js"
+
+import { logOut } from "./modules.js"
+import { findUserData } from "./modules.js"
+
+const auth = getAuth()
+
+const body = document.querySelector("body")
+
+onAuthStateChanged(auth, (user) => {
+	if (user) {
+		const db = getDatabase()
+		const dbRef = ref(db)
+
+		onValue(dbRef, (snapshot) => {
+			const userData = findUserData(snapshot.val(), user.uid)
+			createHeader(user.photoURL, userData.firstName, userData.lastName)
+		})
+	} else {
+		// Caso não tiver um usuário conectado, ele vai para a página de login
+		redirectToLoginPage()
+	}
+})
+
+function createHeader(imageUrl, firstName, lastName) {
+	document.querySelector(".page-skeleton").classList.remove("active")
+	body.style.overflowY = "visible"
+	body.style.pointerEvents = "all"
+
+	const header = document.querySelector("header")
+	header.setAttribute("id", "user-header")
+
+	const navigationUl = document.querySelector("header nav ul")
+	navigationUl.innerHTML = ""
+
+	const signOutBtn = document.createElement("a")
+	signOutBtn.setAttribute("id", "logout")
+	signOutBtn.addEventListener("click", logOut)
+	signOutBtn.textContent = "Sair"
+
+	const userPictureLink = document.createElement("a")
+
+	if (imageUrl !== null) {
+		userPictureLink.innerHTML = `<img src="${imageUrl}" alt="Foto de perfil">`
+		navigationUl.append(signOutBtn, userPictureLink)
+	} else {
+		const image = createProfilePicture(firstName, lastName)
+		navigationUl.append(signOutBtn, image)
+	}
+}
+
+export function redirectToLoginPage() {
+	window.location = "/html/login.html"
+}
+
+function createProfilePicture(firstName, lastName) {
+	const firstLetterOfTheFirstName = firstName.split("")[0].toUpperCase()
+	const firstLetterOfTheLastName = lastName.split("")[0].toUpperCase()
+
+	const image = document.createElement("div")
+	image.classList.add("profilePicture")
+
+	image.textContent = `${firstLetterOfTheFirstName}${firstLetterOfTheLastName}`
+
+	return image
+}
+
+function createPageSkeleton() {
+	const skeleton = document.createElement("div")
+	skeleton.classList.add("page-skeleton", "active")
+
+	skeleton.innerHTML = `
+		<div class="loading"></div>
+	`
+
+	body.style.overflowY = "hidden"
+	body.style.pointerEvents = "none"
+	body.appendChild(skeleton)
+}
+
+createPageSkeleton()
---
+++ js/dashboardTeacher.js
@@ -0,0 +1,279 @@
+import {
+  getDatabase, ref, onValue, set, push, child, get
+} from "https://www.gstatic.com/firebasejs/9.17.1/firebase-database.js";
+
+import {
+  getAuth, onAuthStateChanged,
+} from "https://www.gstatic.com/firebasejs/9.17.1/firebase-auth.js"
+// import { getUserData } from "./modules.js"
+
+const auth = getAuth()
+const db = getDatabase()
+let userId;
+
+
+// ======================================
+// para criar a tabela dos meus alunos
+
+function retrieveData() {
+  const auth = getAuth();
+  const db = getDatabase();
+  let userId;
+
+  onAuthStateChanged(auth, (user) => {
+    userId = user.uid;
+    console.log(userId)
+    if (user) {
+      const professorsRef = ref(db, "/professors/" + userId + "/studentss");
+
+      onValue(professorsRef, (snapshot) => {
+        const studentsIdsObj = snapshot.val();
+        if (studentsIdsObj !== null) {
+          const studentIds = Object.values(studentsIdsObj);
+          console.log("Student IDs:", studentIds);
+
+          createTable(studentIds, db, "studentTable");
+          tableFinancies(studentIds, db, "financies"); // Call tableFinancies here
+          // selectPaymentByTeacherId(userId, studentIds)
+          calculateTotalPaidAmount(userId, studentIds)
+        } else {
+          console.log("Você não possui nenhum aluno");
+        }
+      });
+    }
+  });
+}
+
+
+function createTable(studentIds, db, tableId) {
+  const table = document.getElementById(tableId).getElementsByTagName("tbody")[0];
+  let tbody = table.getElementsByTagName("tbody")[0];
+
+  if (!tbody) {
+    tbody = document.createElement("tbody");
+    table.appendChild(tbody);
+  }
+
+  studentIds.forEach((studentId) => {
+    const studentRef = ref(db, `/students/${studentId}`);
+
+    onValue(studentRef, (snapshot) => {
+      const studentData = snapshot.val();
+      const row = table.insertRow();
+
+
+      const imgCell = row.insertCell();
+      imgCell.classList.add('studentData')
+      imgCell.textContent = "---FOTO---";
+
+      const firstNameCell = row.insertCell();
+      firstNameCell.classList.add('studentData')
+      firstNameCell.textContent = studentData.firstName;
+
+      const lastNameCell = row.insertCell();
+      lastNameCell.classList.add('studentData')
+      lastNameCell.textContent = studentData.lastName;
+
+      const accessPerfilCell = row.insertCell();
+      const btn = document.createElement("a");
+      btn.setAttribute("href", "#");
+      btn.textContent = "Acessar Aluno";
+      accessPerfilCell.appendChild(btn);
+
+    });
+  });
+}
+
+retrieveData()
+// =================================================
+
+function tableFinancies(studentIds, db, tableId) {
+  const table = document.getElementById(tableId).getElementsByTagName("tbody")[0];
+  // verificar se existe o tbody. Se nao existe, cria um elemento tbody
+  if (!tbody) {
+    tbody = document.createElement('tbody');
+    table.appendChild(tbody)
+  }
+
+  studentIds.forEach((studentId) => {
+    // selecionar os dados do firebase
+    const studentRef = ref(db, 'students/' + studentId);
+    onValue(studentRef, (snapshot) => {
+      const data = snapshot.val();
+      if (!data) {
+        console.log("Name:", data.firstName);
+        console.log("data true: " + data)
+      }
+      // console.log("tableId:" + tableId);
+      // console.log("studentId:" + studentId)
+
+      var pendencia = ""
+      if (data) {
+        const firstName = data.firstName;
+        const pendencies = data.pendencies;
+
+        if (pendencies == true) {
+          pendencia = "pago"
+          // console.log('pago')
+        }
+        else {
+          pendencia = "nao pago"
+          // console.log('nao pago')
+        }
+        // console.log("Name:", firstName);
+        // console.log("Pendencies:", pendencies);
+
+      }
+
+      const row = table.insertRow();
+
+      const imgCell = row.insertCell();
+      imgCell.classList.add('studentData')
+      imgCell.textContent = "---FOTO---";
+
+      const firstNameCell = row.insertCell();
+      firstNameCell.classList.add('pendencies')
+      firstNameCell.textContent = data.firstName;
+
+      const lastNameCell = row.insertCell();
+      lastNameCell.classList.add('studentData')
+      lastNameCell.textContent = pendencia;
+
+      const accessPerfilCell = row.insertCell();
+      const btn = document.createElement("a");
+      btn.setAttribute("href", "#");
+      btn.textContent = "Acessar Aluno";
+      accessPerfilCell.appendChild(btn);
+    });
+  });
+}
+
+const studentIds = [];
+
+const tableId = "studentsfinancies";
+tableFinancies(studentIds, db, tableId);
+
+// ===========================================
+
+const addPayment = document.getElementById('payment')
+
+addPayment.addEventListener('click', function(){
+  const studentsId = "";
+  const professorsId = "";
+  console.log("studentId: " + studentsId)
+  console.log("teachertId: " + professorsId)
+
+  const db = getDatabase();
+  const studentRef = ref(db, 'students/')
+  const teacherRef = ref(db, 'professors/')
+  const newPaymentRef = ref(db, 'payments/')
+  const newPayment = push(newPaymentRef)
+  const currentDate = new Date().toISOString().split('T')[0];
+
+  set(newPayment, {
+    teacherId: professorsId,
+    studentId: studentsId,
+    status: 'not paid',
+    date: currentDate,
+    amount: 250,
+  })
+    .then(() => {
+
+      console.log('Payment added successfully.');
+    })
+    .catch((error) => {
+      console.error('Error adding payment:', error);
+    });
+
+  // insertPayments(studentId, teacherId);
+})
+
+// =====================================================
+
+const teste = document.getElementById('testeTotal');
+let total = 0;
+let arr = [];
+const dbRef = ref(getDatabase());
+let paymentid;
+get(child(dbRef, `payments`)).then((snapshot) => {
+  if (snapshot.exists()) {
+    const data = snapshot.val();
+    // console.log("Data:", data); //seleciona todos os nós filhos de payments
+    for (const paymentId in data) {
+      if (data.hasOwnProperty(paymentId)) { //percorre cada id de payments e exibe o id e o amount
+        const payment = data[paymentId];
+        paymentid = paymentId
+        // console.log("Payment id: ", paymentId + " e amount: ", payment.amount);
+        arr.push(payment.amount);
+      }
+    }
+    // console.log(arr);
+    for (let i = 0; i < arr.length; i++) {
+      total += arr[i];
+    }
+    teste.textContent = total;
+    // console.log("Total: " +total); //aqui é o total que deve ser exibido na tela
+  } else {
+    console.log("No data available");
+  }
+}).catch((error) => {
+  console.error(error);
+});
+
+
+// ========================================
+
+
+function calculateTotalPaidAmount(teacherId, studentIds) {
+  const paymentRef = ref(db, 'payments/');
+  let totalPaidAmount = 0;
+
+  onValue(paymentRef, (snapshot) => {
+    const data = snapshot.val();
+    if (data) {
+      const paidStudents = [];
+      const notPaidStudents = [];
+      // Iterate over the payment nodes
+      for (const paymentId in data) {
+        const payment = data[paymentId];
+        const paymentDate = payment.date
+        const studentId = payment.studentId;
+        const status = payment.status;
+        const amount = payment.amount;
+        const teacherId = payment.teacherId
+        
+        
+        // // Check if the payment belongs to the teacher's students
+        // if (status === 'paid' && studentIds.includes(studentId)) {
+        //   // Add the student to the paidStudents array
+          
+        //   paidStudents.push({ studentId, amount, status });
+        //   // Accumulate the amount to calculate the total paid amount
+        //   totalPaidAmount += amount;
+        // }
+
+        if (status === false && studentIds.includes(studentId)) {
+          // Add the student to the paidStudents array
+          notPaidStudents.push({ studentId, amount, status, paymentDate });
+          // Accumulate the amount to calculate the total paid amount
+          // totalPaidAmount += amount;
+        }
+        else if(status !== false && studentIds.includes(studentId)){
+          console.log('paymentId: ' + paymentId)
+          console.log( payment, studentId, status, amount, paymentDate, teacherId)
+        
+          paidStudents.push({studentId, amount, status, paymentDate })
+          totalPaidAmount += amount;
+        }
+      }
+      // Display the paid students and total paid amount
+      console.log('Paid Students:', paidStudents);
+      console.log('Total Paid Amount:', totalPaidAmount);
+      console.log('notPaidStudents: ' , notPaidStudents)
+      console.log('teacherId: '+ teacherId)
+      // You can further process the paid students and total paid amount as needed, such as displaying it on a web page.
+    } else {
+      console.log("No payment found for the specified teacherId.");
+    }
+  });
+}
